ace4721b3fdec6e093f9d993a8dbd8ec7b9f8e14
Jekson365 <jemali.ergemlide.1@btu.edu.ge>
2022-07-01 14:07:38 +0400

    changes

--- game/src/App.js
+++ game/src/App.js
@@ -2,30 +2,9 @@ import "./App.css";
 import { Item } from "./components/Item";
 
 function App() {
-  const items = [
-    {
-      id: Math.floor(Math.random() * 999999999),
-      question: "Question 1",
-      answer:"Answer 1"
-    },
-    {
-      id: Math.floor(Math.random() * 999999999),
-      question: "Question 2",
-      answer:"Answer 2"
-    },
-    {
-      id: Math.floor(Math.random() * 999999999),
-      question: "Question 3",
-      answer:"Answer"
-    },
-  ];
   return(
   <div className="container">
-      {items.map((single) => {
-        return (
-          <Item items={items} />
-        )
-      })}
+      <h1>hello world</h1>
     </div>
   )
 }
